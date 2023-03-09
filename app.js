a1e6d7443149657b3387def6ff5e9573d4226d4c
MinIzL <minilekz@gmail.com>
2023-03-09 21:27:17 +0700

    save

---
+++ app.js
@@ -0,0 +1,16 @@
+const express = require("express");
+const chalk = require("chalk");
+const debug = require("debug")("app");
+const morgan = require("morgan");
+const app = express();
+const port = 3000;
+
+app.use(morgan("combined"));
+
+app.get("/", (req, res) => {
+  res.send("Hello world");
+});
+
+app.listen(port, () => {
+  console.log("Listening on port: " + chalk.green(port));
+});
